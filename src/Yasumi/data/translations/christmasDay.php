0007980b97cec02717f39aa67d0a785c8602946a
Sacha Telgenhof <stelgenhof@gmail.com>
2015-04-13 06:47:21 +0900

    Add translation files for All Saints Dat, Ascension Day, Ash Wednesday, Assumption of Mary, Christmas, Easter and Pentecost.

---
+++ src/Yasumi/data/translations/christmasDay.php
@@ -0,0 +1,19 @@
+<?php
+/*
+ * This file is part of the Yasumi package.
+ *
+ * Copyright (c) 2015 AzuyaLabs
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author Sacha Telgenhof <stelgenhof@gmail.com>
+ *
+ */
+
+// Translation for Christmas Day
+return [
+    'en_US' => 'Christmas',
+    'nl_NL' => 'Kerstmis',
+    'nl_BE' => 'Kerstmis'
+];
---
+++ src/Yasumi/data/translations/easterMonday.php
@@ -0,0 +1,19 @@
+<?php
+/*
+ * This file is part of the Yasumi package.
+ *
+ * Copyright (c) 2015 AzuyaLabs
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author Sacha Telgenhof <stelgenhof@gmail.com>
+ *
+ */
+
+// Translation for Easter Monday
+return [
+    'en_US' => 'Easter Monday',
+    'nl_NL' => 'Tweede Paasdag',
+    'nl_BE' => 'Paasmaandag'
+];
