52ebe2c454aaaa5ef06ef9f027e4d4c0b8fbed4a
Sacha Telgenhof <stelgenhof@gmail.com>
2016-04-10 22:55:00 +0900

    Fixed test case for Sweden's Midsummer's Day (st. Johns Day) as it was calculated to be always on June 24th. However the holiday has always been on a Saturday (between June 20 and June 26).

--- tests/Sweden/SwedenBaseTestCase.php
+++ tests/Sweden/SwedenBaseTestCase.php
@@ -13,7 +13,7 @@
 namespace Yasumi\tests\Sweden;
 
 use PHPUnit_Framework_TestCase;
-use Yasumi\Tests\YasumiBase;
+use Yasumi\tests\YasumiBase;
 
 /**
  * Base class for test cases of the Sweden holiday provider.
