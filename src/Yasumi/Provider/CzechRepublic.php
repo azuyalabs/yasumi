ce52557b078d6c3af2c8f0a2667b820b3d013075
Dennis Fridrich <fridrich.dennis@gmail.com>
2016-05-04 11:35:43 +0200

    Fix author

--- src/Yasumi/Provider/CzechRepublic.php
+++ src/Yasumi/Provider/CzechRepublic.php
@@ -1,16 +1,5 @@
 <?php
 
-/**
- *  This file is part of the Yasumi package.
- *
- *  Copyright (c) 2015 - 2016 AzuyaLabs
- *
- *  For the full copyright and license information, please view the LICENSE
- *  file that was distributed with this source code.
- *
- * @author Sacha Telgenhof <stelgenhof@gmail.com>
- */
-
 namespace Yasumi\Provider;
 
 use DateTime;
--- tests/CzechRepublic/CzechStateHoodDayTest.php
+++ tests/CzechRepublic/CzechStateHoodDayTest.php
@@ -1,15 +1,4 @@
 <?php
-/**
- *  This file is part of the Yasumi package.
- *
- *  Copyright (c) 2015 - 2016 AzuyaLabs
- *
- *  For the full copyright and license information, please view the LICENSE
- *  file that was distributed with this source code.
- *
- * @author Sacha Telgenhof <stelgenhof@gmail.com>
- */
-
 namespace Yasumi\tests\CzechRepublic;
 
 use DateTime;
