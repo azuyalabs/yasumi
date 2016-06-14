f69fefd97be393db26a7da09e429dcdf47e3beab
Sacha Telgenhof <stelgenhof@gmail.com>
2016-06-14 11:49:02 +0900

    Added Brandenburg Holiday Provider.

---
+++ tests/Germany/Brandenburg/BrandenburgBaseTestCase.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ *  This file is part of the Yasumi package.
+ *
+ *  Copyright (c) 2015 - 2016 AzuyaLabs
+ *
+ *  For the full copyright and license information, please view the LICENSE
+ *  file that was distributed with this source code.
+ *
+ * @author Sacha Telgenhof <stelgenhof@gmail.com>
+ */
+
+namespace Yasumi\tests\Germany\Brandenburg;
+
+use Yasumi\tests\Germany\GermanyBaseTestCase;
+use Yasumi\tests\YasumiBase;
+
+/**
+ * Base class for test cases of the Brandenburg (Germany) holiday provider.
+ */
+abstract class BrandenburgBaseTestCase extends GermanyBaseTestCase
+{
+    use YasumiBase;
+
+    /**
+     * Name of the region (e.g. country / state) to be tested
+     */
+    const REGION = 'Germany/Brandenburg';
+
+    /**
+     * Timezone in which this provider has holidays defined
+     */
+    const TIMEZONE = 'Europe/Berlin';
+}
---
+++ tests/Germany/Brandenburg/BrandenburgTest.php
@@ -0,0 +1,87 @@
+<?php
+/**
+ *  This file is part of the Yasumi package.
+ *
+ *  Copyright (c) 2015 - 2016 AzuyaLabs
+ *
+ *  For the full copyright and license information, please view the LICENSE
+ *  file that was distributed with this source code.
+ *
+ * @author Sacha Telgenhof <stelgenhof@gmail.com>
+ */
+
+namespace Yasumi\tests\Germany\Brandenburg;
+
+use Yasumi\Holiday;
+
+/**
+ * Class for testing holidays in Brandenburg (Germany)
+ */
+class BrandenburgTest extends BrandenburgBaseTestCase
+{
+    /**
+     * @var int year random year number used for all tests in this Test Case
+     */
+    protected $year;
+
+    /**
+     * Tests if all national holidays in Brandenburg (Germany) are defined by the provider class
+     */
+    public function testNationalHolidays()
+    {
+        $this->assertDefinedHolidays([
+            'newYearsDay',
+            'goodFriday',
+            'easter',
+            'easterMonday',
+            'internationalWorkersDay',
+            'ascensionDay',
+            'pentecost',
+            'pentecostMonday',
+            'germanUnityDay',
+            'reformationDay',
+            'christmasDay',
+            'secondChristmasDay'
+        ], self::REGION, $this->year, Holiday::TYPE_NATIONAL);
+    }
+
+    /**
+     * Tests if all observed holidays in Brandenburg (Germany) are defined by the provider class
+     */
+    public function testObservedHolidays()
+    {
+        $this->assertDefinedHolidays([], self::REGION, $this->year, Holiday::TYPE_OBSERVANCE);
+    }
+
+    /**
+     * Tests if all seasonal holidays in Brandenburg (Germany) are defined by the provider class
+     */
+    public function testSeasonalHolidays()
+    {
+        $this->assertDefinedHolidays([], self::REGION, $this->year, Holiday::TYPE_SEASON);
+    }
+
+    /**
+     * Tests if all bank holidays in Brandenburg (Germany) are defined by the provider class
+     */
+    public function testBankHolidays()
+    {
+        $this->assertDefinedHolidays([], self::REGION, $this->year, Holiday::TYPE_BANK);
+    }
+
+    /**
+     * Tests if all other holidays in Brandenburg (Germany) are defined by the provider class
+     */
+    public function testOtherHolidays()
+    {
+        $this->assertDefinedHolidays([], self::REGION, $this->year, Holiday::TYPE_OTHER);
+    }
+
+    /**
+     * Initial setup of this Test Case
+     */
+    protected function setUp()
+    {
+        $this->year = $this->generateRandomYear(1990);
+    }
+}
