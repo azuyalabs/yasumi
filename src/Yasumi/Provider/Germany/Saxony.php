579e57b2f1bd9c7fb41c23c64b6182159e23c79c
Sacha Telgenhof <stelgenhof@gmail.com>
2016-06-17 17:29:54 +0900

    Add the Saxon (Germany) Holiday Provider.

---
+++ src/Yasumi/Provider/Germany/Saxony.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ *  This file is part of the Yasumi package.
+ *
+ *  Copyright (c) 2015 - 2016 AzuyaLabs
+ *
+ *  For the full copyright and license information, please view the LICENSE
+ *  file that was distributed with this source code.
+ *
+ *  @author Sacha Telgenhof <stelgenhof@gmail.com>
+ */
+
+namespace Yasumi\Provider\Germany;
+
+use Yasumi\Provider\ChristianHolidays;
+use Yasumi\Provider\Germany;
+
+/**
+ * Provider for all holidays in Saxony (Germany).
+ *
+ * The Free State of Saxony (German: Freistaat Sachsen) is a landlocked federal state of Germany, bordering the federal
+ * states of Brandenburg, Saxony Anhalt, Thuringia, and Bavaria, as well as the countries of Poland and the Czech 
+ * Republic. Its capital is Dresden, and its largest city is Leipzig.
+ *
+ * @link https://en.wikipedia.org/wiki/Saxony
+ */
+class Saxony extends Germany
+{
+    /**
+     * Code to identify this Holiday Provider. Typically this is the ISO3166 code corresponding to the respective
+     * country or subregion.
+     */
+    const ID = 'DE_SN';
+
+    /**
+     * Initialize holidays for Saxony (Germany).
+     */
+    public function initialize()
+    {
+        parent::initialize();
+
+        // Add custom Christian holidays
+        $this->calculateReformationDay();
+    }
+}
