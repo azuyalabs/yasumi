cdbb2754e83948eeadac0fb60576c90a57888457
Sacha Telgenhof Oude Koehorst <stelgenhof@gmail.com>
2016-12-07 09:56:55 +0900

    Initial commit for the Ireland Holiday Provider

---
+++ tests/Ireland/IrelandBaseTestCase.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ *  This file is part of the Yasumi package.
+ *
+ *  Copyright (c) 2015 - 2016 AzuyaLabs
+ *
+ *  For the full copyright and license information, please view the LICENSE
+ *  file that was distributed with this source code.
+ *
+ * @author Sacha Telgenhof <stelgenhof@gmail.com>
+ */
+
+namespace Yasumi\tests\Ireland;
+
+use PHPUnit_Framework_TestCase;
+use Yasumi\tests\YasumiBase;
+
+/**
+ * Base class for test cases of the Ireland holiday provider.
+ */
+abstract class IrelandBaseTestCase extends PHPUnit_Framework_TestCase
+{
+    use YasumiBase;
+
+    /**
+     * Name of the region (e.g. country / state) to be tested
+     */
+    const REGION = 'Ireland';
+
+    /**
+     * Timezone in which this provider has holidays defined
+     */
+    const TIMEZONE = 'Europe/Dublin';
+
+    /**
+     * Locale that is considered common for this provider
+     */
+    const LOCALE = 'en_IE';
+}
