6ab0aee4e7ebeeee2fa5d618e3a9e813d87948eb
Sacha Telgenhof <stelgenhof@gmail.com>
2017-01-26 10:22:50 +0900

    Adjusted unit test and calculation of Reformation Day in the German state of Saxony-Anhalt as it has been established since 1517.

--- src/Yasumi/Provider/Germany/SaxonyAnhalt.php
+++ src/Yasumi/Provider/Germany/SaxonyAnhalt.php
@@ -41,6 +41,19 @@ class SaxonyAnhalt extends Germany
 
         // Add custom Christian holidays
         $this->addHoliday($this->epiphany($this->year, $this->timezone, $this->locale, Holiday::TYPE_OTHER));
+        $this->calculateReformationDay();
+    }
+
+    /**
+     * For the German state of Saxony-Anhalt, Reformation Day was celebrated since 1517.
+     * Note: In 2017 all German states will celebrate Reformation Day for its 500th anniversary.
+     */
+    private function calculateReformationDay()
+    {
+        if ($this->year < 1517) {
+            return;
+        }
+
         $this->addHoliday($this->reformationDay($this->year, $this->timezone, $this->locale));
     }
 }
