20c0c244a97e705051101ad9219cdc481c1a6c03
Gedas Lukošius <gedas@lukosius.me>
2017-12-20 09:52:39 +0200

    Add tests for Estonia holidays

---
+++ tests/Estonia/GoodFridayDayTest.php
@@ -0,0 +1,79 @@
+<?php
+
+/**
+ * This file is part of the Yasumi package.
+ *
+ * Copyright (c) 2015 - 2017 AzuyaLabs
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author Sacha Telgenhof <stelgenhof@gmail.com>
+ */
+
+namespace Yasumi\tests\Estonia;
+
+use Yasumi\Holiday;
+use Yasumi\tests\YasumiTestCaseInterface;
+
+/**
+ * Class containing tests for Estonia's Good Friday day.
+ *
+ * @author Gedas Lukošius <gedas@lukosius.me>
+ */
+class GoodFridayDayTest extends EstoniaBaseTestCase implements YasumiTestCaseInterface
+{
+    /**
+     * The name of the holiday to be tested
+     */
+    const HOLIDAY = 'goodFriday';
+
+    /**
+     * @return array
+     *
+     * @throws \Exception
+     */
+    public function holidayDataProvider()
+    {
+        return $this->generateRandomGoodFridayDates(self::TIMEZONE);
+    }
+
+    /**
+     * Test defined holiday in the test
+     *
+     * @dataProvider holidayDataProvider
+     *
+     * @param int    $year     the year for which the holiday defined in this test needs to be tested
+     * @param string $expected the expected date
+     */
+    public function testHoliday($year, $expected)
+    {
+        $this->assertHoliday(
+            self::REGION,
+            self::HOLIDAY,
+            $year,
+            new \DateTime($expected, new \DateTimeZone(self::TIMEZONE))
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function testTranslation()
+    {
+        $this->assertTranslatedHolidayName(
+            self::REGION,
+            self::HOLIDAY,
+            $this->generateRandomYear(),
+            [self::LOCALE => 'Suur Reede']
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function testHolidayType()
+    {
+        $this->assertHolidayType(self::REGION, self::HOLIDAY, $this->generateRandomYear(), Holiday::TYPE_OFFICIAL);
+    }
+}
