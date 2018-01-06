7d789c57a37a45f78501b766772c71b3cbc9934e
Gedas Lukošius <gedas@lukosius.me>
2018-01-06 05:37:21 +0200

    Provider for holidays in Russia  (#72)

---
+++ tests/Russia/RussiaBaseTestCase.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * This file is part of the Yasumi package.
+ *
+ * Copyright (c) 2015 - 2017 AzuyaLabs
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author Sacha Telgenhof <stelgenhof@gmail.com>
+ */
+
+namespace Yasumi\tests\Russia;
+
+use Yasumi\tests\YasumiBase;
+
+/**
+ * Base class for test cases of the Russia holiday provider.
+ *
+ * @author Gedas Lukošius <gedas@lukosius.me>
+ */
+abstract class RussiaBaseTestCase extends \PHPUnit_Framework_TestCase
+{
+    use YasumiBase;
+
+    /**
+     * Name of the country to be tested
+     */
+    const REGION = 'Russia';
+
+    /**
+     * Timezone in which this provider has holidays defined
+     */
+    const TIMEZONE = 'Europe/Moscow';
+
+    /**
+     * Locale that is considered common for this provider
+     */
+    const LOCALE = 'ru_RU';
+}
