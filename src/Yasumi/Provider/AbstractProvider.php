6bcfbda25cd36eca22a31eb3322d54343dae2529
Sacha Telgenhof <stelgenhof@gmail.com>
2018-01-21 15:53:03 +0900

    Fix for empty variable check.
    Signed-off-by: Sacha Telgenhof <stelgenhof@gmail.com>

--- src/Yasumi/Provider/AbstractProvider.php
+++ src/Yasumi/Provider/AbstractProvider.php
@@ -177,7 +177,8 @@ abstract class AbstractProvider implements ProviderInterface, Countable, Iterato
         // If no data is defined for this Holiday Provider, the function falls back to the global weekend definition.
         // @TODO Ideally avoid late static binding here (static::ID)
         if ($date instanceof DateTime) {
-            $weekend_days = isset(self::WEEKEND_DATA[static::ID]) ? self::WEEKEND_DATA[static::ID] : [0, 6];
+            $weekend_days = !empty(self::WEEKEND_DATA[static::ID]) ? self::WEEKEND_DATA[static::ID] : [0, 6];
+
             if (in_array((int)$date->format('w'), $weekend_days, true)) {
                 return false;
             }
