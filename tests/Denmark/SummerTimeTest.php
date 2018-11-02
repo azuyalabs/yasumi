74e52b20f14f2db50f9e48c914dc12d3d33a650e
Sacha Telgenhof <stelgenhof@gmail.com>
2018-11-02 12:22:53 +0900

    Since 1980 Summertime in Denmark starts on the last day of March. In 1980 itself however, it started on April, 6th. Adjusted the test accordingly to avoid a false negative.
    Signed-off-by: Sacha Telgenhof <stelgenhof@gmail.com>

--- tests/Denmark/SummerTimeTest.php
+++ tests/Denmark/SummerTimeTest.php
@@ -35,11 +35,18 @@ class SummerTimeTest extends DenmarkBaseTestCase implements YasumiTestCaseInterf
         $this->assertNotHoliday(self::REGION, self::HOLIDAY, $this->generateRandomYear(1949, 1979));
 
         $year = $this->generateRandomYear(1980, 2036);
+        $expectedDate = new DateTime("last sunday of march $year", new DateTimeZone(self::TIMEZONE));
+
+        // Since 1980 Summertime in Denmark starts on the last day of March. In 1980 itself however, it started on April, 6th.
+        if ($year === 1980) {
+            $expectedDate = new DateTime('1980-04-06', new DateTimeZone(self::TIMEZONE));
+        }
+
         $this->assertHoliday(
             self::REGION,
             self::HOLIDAY,
             $year,
-            new DateTime("last sunday of march $year", new DateTimeZone(self::TIMEZONE))
+            $expectedDate
         );
     }
 
