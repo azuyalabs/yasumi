7e2a8d14f94381b37219fcb7e98a9b91d983df1d
Sacha Telgenhof <stelgenhof@gmail.com>
2019-09-09 00:32:37 +0900

    Reference classes by their class names rather than magic constants.
    Signed-off-by: Sacha Telgenhof <stelgenhof@gmail.com>

--- src/Yasumi/Provider/AbstractProvider.php
+++ src/Yasumi/Provider/AbstractProvider.php
@@ -155,7 +155,7 @@ abstract class AbstractProvider implements ProviderInterface, Countable, Iterato
         }
 
         $this->holidays[$holiday->shortName] = $holiday;
-        \uasort($this->holidays, [__CLASS__, 'compareDates']);
+        \uasort($this->holidays, [AbstractProvider::class, 'compareDates']);
     }
 
 
