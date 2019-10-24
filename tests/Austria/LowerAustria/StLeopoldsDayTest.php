c158af1bd732decd3e1aa6528b11d2fb3302f64b
Alex <aprogs@2281272.no-reply.drupal.org>
2019-10-24 15:43:18 +0300

    Implemented tests for holiday providers that belong to states of Austria.

---
+++ tests/Austria/LowerAustria/StLeopoldsDayTest.php
@@ -0,0 +1,79 @@
+<?php declare(strict_types=1);
+/**
+ * This file is part of the Yasumi package.
+ *
+ * Copyright (c) 2015 - 2019 AzuyaLabs
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author Sacha Telgenhof <me@sachatelgenhof.com>
+ */
+
+namespace Yasumi\tests\Austria\LowerAustria;
+
+use DateTime;
+use Exception;
+use ReflectionException;
+use Yasumi\Holiday;
+use Yasumi\tests\YasumiTestCaseInterface;
+
+/**
+ * Class for testing Saint Leopold's Day in Lower Austria (Austria).
+ */
+class StLeopoldsDayTest extends LowerAustriaBaseTestCase implements YasumiTestCaseInterface
+{
+    /**
+     * The name of the holiday.
+     */
+    public const HOLIDAY = 'stLeopoldsDay';
+
+    /**
+     * Tests Saint Leopold's Day.
+     *
+     * @dataProvider StLeopoldsDayDataProvider
+     *
+     * @param int $year the year for which Saint Leopold's Day needs to be tested.
+     * @param DateTime $expected the expected date.
+     *
+     * @throws ReflectionException
+     */
+    public function testStLeopoldsDay($year, $expected)
+    {
+        $this->assertHoliday(self::REGION, self::HOLIDAY, $year, $expected);
+    }
+
+    /**
+     * Returns a list of random test dates used for assertion of Saint Leopold's Day.
+     *
+     * @return array list of test dates for Saint Leopold's Day.
+     * @throws Exception
+     */
+    public function StLeopoldsDayDataProvider(): array
+    {
+        return $this->generateRandomDates(11, 15, self::TIMEZONE);
+    }
+
+    /**
+     * Tests translated name of the holiday defined in this test.
+     * @throws ReflectionException
+     */
+    public function testTranslation(): void
+    {
+        $this->assertTranslatedHolidayName(
+            self::REGION,
+            self::HOLIDAY,
+            $this->generateRandomYear(),
+            [self::LOCALE => 'Leopold']
+        );
+    }
+
+    /**
+     * Tests type of the holiday defined in this test.
+     * @throws ReflectionException
+     */
+    public function testHolidayType(): void
+    {
+        $this->assertHolidayType(self::REGION, self::HOLIDAY, $this->generateRandomYear(), Holiday::TYPE_OFFICIAL);
+    }
+}
