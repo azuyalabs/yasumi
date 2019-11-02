1a0992e4893644e62d227a4fb75d2e71f19bf2fd
Sacha Telgenhof <me@sachatelgenhof.com>
2019-11-02 13:45:45 +0900

    Refactored check so it checks the instance type. A boolean as the initial data type is not correct in this situation.
    Signed-off-by: Sacha Telgenhof <stelgenhof@gmail.com>

--- src/Yasumi/Yasumi.php
+++ src/Yasumi/Yasumi.php
@@ -88,11 +88,11 @@ class Yasumi
         // Setup start date, if its an instance of \DateTime, clone to prevent modification to original
         $date = $startDate instanceof \DateTime ? clone $startDate : $startDate;
 
-        $provider = false;
+        $provider = null;
 
         while ($workingDays > 0) {
             $date = $date->add(new \DateInterval('P1D'));
-            if (!$provider || $provider->getYear() !== \getdate()['year']) {
+            if (! $provider instanceof ProviderInterface || $provider->getYear() !== \getdate()['year']) {
                 $provider = self::create($class, (int)$date->format('Y'));
             }
             if ($provider->isWorkingDay($date)) {
@@ -282,11 +282,11 @@ class Yasumi
         // Setup start date, if its an instance of \DateTime, clone to prevent modification to original
         $date = $startDate instanceof \DateTime ? clone $startDate : $startDate;
 
-        $provider = false;
+        $provider = null;
 
         while ($workingDays > 0) {
             $date = $date->sub(new \DateInterval('P1D'));
-            if (!$provider || $provider->getYear() !== \getdate()['year']) {
+            if (! $provider instanceof ProviderInterface || $provider->getYear() !== \getdate()['year']) {
                 $provider = self::create($class, (int)$date->format('Y'));
             }
             if ($provider->isWorkingDay($date)) {
