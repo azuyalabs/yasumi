47eb2d6da0d87bd47623b97bc0598f8151cd5969
Sacha Telgenhof <me@sachatelgenhof.com>
2021-01-12 22:25:33 +0900

    Updated copyright year.

--- src/Yasumi/Provider/Luxembourg.php
+++ src/Yasumi/Provider/Luxembourg.php
@@ -1,4 +1,15 @@
 <?php
+/*
+ * This file is part of the Yasumi package.
+ *
+ * Copyright (c) 2015 - 2021 AzuyaLabs
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author Sacha Telgenhof <me@sachatelgenhof.com>
+ */
+
 declare(strict_types=1);
 
 namespace Yasumi\Provider;
