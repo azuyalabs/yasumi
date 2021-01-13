3c81b43927d5a963140c4462a3d339ee9b06b1f4
Sacha Telgenhof <me@sachatelgenhof.com>
2021-01-14 00:12:34 +0900

    Avoid overriding the same variable.

--- src/Yasumi/SubstituteHoliday.php
+++ src/Yasumi/SubstituteHoliday.php
@@ -112,8 +112,8 @@ class SubstituteHoliday extends Holiday
         $name = parent::getName();
 
         if ($name === $this->getKey()) {
-            foreach ($this->getLocales($locales) as $locales) {
-                $pattern = $this->substituteHolidayTranslations[$locales] ?? null;
+            foreach ($this->getLocales($locales) as $localeList) {
+                $pattern = $this->substituteHolidayTranslations[$localeList] ?? null;
                 if ($pattern) {
                     return \str_replace('{0}', $this->substitutedHoliday->getName(), $pattern);
                 }
