59e3834a467d37caff0f414183c5dcc9c5bd53ef
Sacha Telgenhof <me@sachatelgenhof.com>
2021-05-04 11:11:59 +0900

    Reformatting
    Signed-off-by: Sacha Telgenhof <me@sachatelgenhof.com>

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -1,4 +1,5 @@
 <?php declare(strict_types=1);
+
 /**
  * This file is part of the Yasumi package.
  *
@@ -9,25 +10,27 @@
  *
  * @author Sacha Telgenhof <me@sachatelgenhof.com>
  */
-
 $finder = PhpCsFixer\Finder::create()->in(__DIR__);
 
-return PhpCsFixer\Config::create()->setRiskyAllowed(true)->setRules([
-        '@PSR2'                             => true,
-        '@Symfony'                          => true,
-        'array_syntax'                      => ['syntax' => 'short'],
-        'native_function_invocation'        => true,
-        'blank_line_after_opening_tag'      => true,
-        'is_null'                           => true,
-        'modernize_types_casting'           => true,
-        'self_accessor'                     => true,
-        'dir_constant'                      => true,
-        'ordered_class_elements'            => true,
-        'ordered_imports'                   => ['sort_algorithm' => 'alpha'],
-        'no_unused_imports'                 => true,
-        'single_quote'                      => true,
-        'space_after_semicolon'             => true,
-        'trailing_comma_in_multiline'       => true,
-        'cast_spaces'                       => ['space' => 'single'],
-        'declare_strict_types'              => true,
-    ])->setLineEnding("\n")->setFinder($finder);
+$config = new PhpCsFixer\Config();
+$config->setRiskyAllowed(true)->setRules([
+    '@PSR2' => true,
+    '@Symfony' => true,
+    'array_syntax' => ['syntax' => 'short'],
+    'native_function_invocation' => true,
+    'blank_line_after_opening_tag' => true,
+    'is_null' => true,
+    'modernize_types_casting' => true,
+    'self_accessor' => true,
+    'dir_constant' => true,
+    'ordered_class_elements' => true,
+    'ordered_imports' => ['sort_algorithm' => 'alpha'],
+    'no_unused_imports' => true,
+    'single_quote' => true,
+    'space_after_semicolon' => true,
+    'trailing_comma_in_multiline' => true,
+    'cast_spaces' => ['space' => 'single'],
+    'declare_strict_types' => true,
+])->setLineEnding("\n")->setFinder($finder);
+
+return $config;
