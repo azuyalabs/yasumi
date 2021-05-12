e9f183744694eb0dc1c506bd90beabb3220bf346
Sacha Telgenhof <me@sachatelgenhof.com>
2021-05-12 09:06:32 +0900

    Added external sources for Austria.
    Signed-off-by: Sacha Telgenhof <me@sachatelgenhof.com>

--- tests/Austria/AustriaTest.php
+++ tests/Austria/AustriaTest.php
@@ -16,11 +16,12 @@ namespace Yasumi\tests\Austria;
 
 use ReflectionException;
 use Yasumi\Holiday;
+use Yasumi\tests\ProviderTestCase;
 
 /**
  * Class for testing holidays in Austria.
  */
-class AustriaTest extends AustriaBaseTestCase
+class AustriaTest extends AustriaBaseTestCase implements ProviderTestCase
 {
     /**
      * @var int year random year number used for all tests in this Test Case
@@ -97,5 +98,13 @@ class AustriaTest extends AustriaBaseTestCase
     public function testOtherHolidays(): void
     {
         $this->assertDefinedHolidays([], self::REGION, $this->year, Holiday::TYPE_OTHER);
+    }
+
+    /**
+     * @throws ReflectionException
+     */
+    public function testSources(): void
+    {
+        $this->assertSources(self::REGION, 2);
     }
 }
