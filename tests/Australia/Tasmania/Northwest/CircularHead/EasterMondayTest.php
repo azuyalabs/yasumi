ede26b499876847a0e4d75e912762033473a9da5
Sacha Telgenhof <me@sachatelgenhof.com>
2022-01-17 23:40:00 +0900

    Updated copyright year.

--- tests/Australia/Tasmania/Northwest/CircularHead/EasterMondayTest.php
+++ tests/Australia/Tasmania/Northwest/CircularHead/EasterMondayTest.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 /*
  * This file is part of the Yasumi package.
  *
- * Copyright (c) 2015 - 2021 AzuyaLabs
+ * Copyright (c) 2015 - 2022 AzuyaLabs
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
