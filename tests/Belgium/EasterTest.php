a71e6c383c1ff598e0d7273d1f8df2adab03d429
Kerstens Maxim <maxim.kerstens@gmail.com>
2022-03-21 09:09:47 +0100

    test: Easter is an observed holiday

--- tests/Belgium/EasterTest.php
+++ tests/Belgium/EasterTest.php
@@ -68,6 +68,6 @@ class EasterTest extends BelgiumBaseTestCase implements HolidayTestCase
      */
     public function testHolidayType(): void
     {
-        $this->assertHolidayType(self::REGION, self::HOLIDAY, $this->generateRandomYear(), Holiday::TYPE_OFFICIAL);
+        $this->assertHolidayType(self::REGION, self::HOLIDAY, $this->generateRandomYear(), Holiday::TYPE_OBSERVANCE);
     }
 }
