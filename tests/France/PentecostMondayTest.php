538a89af96eb5bb0eb1833228fb59969a611b36b
Sacha Telgenhof <me@sachatelgenhof.com>
2022-06-14 01:31:54 +0900

    Altered test to include condition that Pentecost Monday was only a holiday up to 2004.
    Signed-off-by: Sacha Telgenhof <me@sachatelgenhof.com>

--- tests/France/PentecostMondayTest.php
+++ tests/France/PentecostMondayTest.php
@@ -22,6 +22,12 @@ use Yasumi\tests\HolidayTestCase;
 
 /**
  * Class for testing Pentecost Monday in France.
+ *
+ * Until 2004, Pentecost Monday was an official holiday. Since 2004, the holiday is considered a 'working holiday',
+ * imposed by law to be by default on Pentecost Monday. Pentecost Monday is still a holiday (but a working holiday).
+ *
+ * @see: https://en.wikipedia.org/wiki/Journ%C3%A9e_de_solidarit%C3%A9_envers_les_personnes_%C3%A2g%C3%A9es
+ * @see: https://fr.wikipedia.org/w/index.php?title=Journ%C3%A9e_de_solidarit%C3%A9_envers_les_personnes_%C3%A2g%C3%A9es_et_handicap%C3%A9es&tableofcontents=0
  */
 class PentecostMondayTest extends FranceBaseTestCase implements HolidayTestCase
 {
@@ -29,6 +35,8 @@ class PentecostMondayTest extends FranceBaseTestCase implements HolidayTestCase
      * The name of the holiday.
      */
     public const HOLIDAY = 'pentecostMonday';
+
+    public const EST_YEAR_DAY_OF_SOLIDARITY_WITH_ELDERLY = 2004;
 
     /**
      * Tests Pentecost Monday.
@@ -68,6 +76,18 @@ class PentecostMondayTest extends FranceBaseTestCase implements HolidayTestCase
      */
     public function testHolidayType(): void
     {
-        $this->assertHolidayType(self::REGION, self::HOLIDAY, $this->generateRandomYear(), Holiday::TYPE_OFFICIAL);
+        $this->assertHolidayType(
+            self::REGION,
+            self::HOLIDAY,
+            $this->generateRandomYear(null, self::EST_YEAR_DAY_OF_SOLIDARITY_WITH_ELDERLY - 1),
+            Holiday::TYPE_OFFICIAL
+        );
+
+        $this->assertHolidayType(
+            self::REGION,
+            self::HOLIDAY,
+            $this->generateRandomYear(self::EST_YEAR_DAY_OF_SOLIDARITY_WITH_ELDERLY),
+            Holiday::TYPE_OBSERVANCE
+        );
     }
 }
