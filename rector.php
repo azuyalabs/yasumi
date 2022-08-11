19ca165a6eedfa70f9b9d328ec2f25bfb30e05ed
Sacha Telgenhof <me@sachatelgenhof.com>
2022-08-11 20:14:10 +0900

    Included unit tests to be checked by rector.
    Signed-off-by: Sacha Telgenhof <me@sachatelgenhof.com>

--- rector.php
+++ rector.php
@@ -9,6 +9,7 @@ use Rector\Set\ValueObject\SetList;
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->paths([
         __DIR__.'/src',
+        __DIR__.'/tests',
     ]);
 
     // single rules
