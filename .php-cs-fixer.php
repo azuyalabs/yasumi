d2dbc61517630b3d25e6eadec1b9206834adf6e6
Sacha Telgenhof <me@sachatelgenhof.com>
2023-01-08 22:01:12 +0900

    Reordered rules list alphabetically.
    Signed-off-by: Sacha Telgenhof <me@sachatelgenhof.com>

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -14,17 +14,17 @@ $finder = PhpCsFixer\Finder::create()->in(__DIR__);
 
 $config = new PhpCsFixer\Config();
 $config->setRiskyAllowed(true)->setRules([
-    '@Symfony' => true,
-    'blank_line_after_opening_tag' => true,
-    'is_null' => true,
-    'modernize_types_casting' => true,
-    'self_accessor' => true,
-    'dir_constant' => true,
-    'ordered_class_elements' => true,
-    'declare_strict_types' => true,
-    'no_superfluous_elseif' => true,
-    'combine_consecutive_issets' => true,
-    'combine_consecutive_unsets' => true,
+  '@Symfony' => true,
+  'blank_line_after_opening_tag' => true,
+  'combine_consecutive_issets' => true,
+  'combine_consecutive_unsets' => true,
+  'declare_strict_types' => true,
+  'dir_constant' => true,
+  'is_null' => true,
+  'modernize_types_casting' => true,
+  'no_superfluous_elseif' => true,
+  'ordered_class_elements' => true,
+  'self_accessor' => true,
 ])->setFinder($finder);
 
 return $config;
