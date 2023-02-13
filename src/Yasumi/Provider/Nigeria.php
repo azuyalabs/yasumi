fc26e3266662fc315f7883eb77a4788d882fba8f
Manomite <manomitehq@gmail.com>
2023-02-13 15:43:00 +0100

    Added Nigeria

--- src/Yasumi/Provider/Nigeria.php
+++ src/Yasumi/Provider/Nigeria.php
@@ -60,16 +60,22 @@ class Nigeria extends AbstractProvider
         $this->addHoliday($this->christmasDay($this->year, $this->timezone, $this->locale));
         $this->addHoliday($this->secondChristmasDay($this->year, $this->timezone, $this->locale));
         $this->addHoliday($this->easterMonday($this->year, $this->timezone, $this->locale));
-        $this->addHoliday($this->valentinesDay($this->year, $this->timezone, $this->locale));
-        $this->addHoliday($this->labourDay($this->year, $this->timezone, $this->locale));
+        $this->addHoliday($this->valentinesDay($this-ti>year, $this->mezone, $this->locale));
         $this->addHoliday($this->fathersDay($this->year, $this->timezone, $this->locale));
         $this->addHoliday($this->internationalWomensDay($this->year, $this->timezone, $this->locale));
         $this->addHoliday($this->mothersDay($this->year, $this->timezone, $this->locale));
         $this->addHoliday($this->mothersDay($this->year, $this->timezone, $this->locale));
+        $this->addHoliday($this->pentecost($this->year, $this->timezone, $this->locale, Holiday::TYPE_OBSERVANCE));
+        $this->addHoliday($this->pentecostMonday($this->year, $this->timezone, $this->locale));
+        $this->addHoliday($this->corpusChristi($this->year, $this->timezone, $this->locale, Holiday::TYPE_OFFICIAL));
        
         // Calculate other holidays
         $this->calculateIndependenceDay();
-       
+        $this->childrenDay();
+        $this->workersDay();
+        $this->democracyDay();
+        $this->youthDay();
+        $this->eidDay(); 
        
     }
 
@@ -97,6 +103,51 @@ class Nigeria extends AbstractProvider
             $this->addHoliday(new Holiday('independenceDay', [
                 'en' => 'Independence Day',
             ], new DateTime("$this->year-10-1", DateTimeZoneFactory::getDateTimeZone($this->timezone)), $this->locale));
+        }
+    }
+
+    private function childrenDay(): void
+    {
+        if ($this->year >= 1960) {
+            $this->addHoliday(new Holiday('children\'sDay', [
+                'en' => 'Children Day',
+            ], new DateTime("$this->year-05-27", DateTimeZoneFactory::getDateTimeZone($this->timezone)), $this->locale));
+        }
+    }
+
+    private function workersDay(): void
+    {
+        if ($this->year >= 1960) {
+            $this->addHoliday(new Holiday('workers\'sDay', [
+                'en' => 'Workers Day',
+            ], new DateTime("$this->year-05-01", DateTimeZoneFactory::getDateTimeZone($this->timezone)), $this->locale));
+        }
+    }
+
+    private function democracyDay(): void
+    {
+        if ($this->year >= 1960) {
+            $this->addHoliday(new Holiday('democracyDay', [
+                'en' => 'Democracy Day',
+            ], new DateTime("$this->year-06-12", DateTimeZoneFactory::getDateTimeZone($this->timezone)), $this->locale));
+        }
+    }
+
+    private function youthDay(): void
+    {
+        if ($this->year >= 2020) {
+            $this->addHoliday(new Holiday('youthDay', [
+                'en' => 'National Youth Day',
+            ], new DateTime("$this->year-11-01", DateTimeZoneFactory::getDateTimeZone($this->timezone)), $this->locale));
+        }
+    }
+
+    private function eidDay(): void
+    {
+        if ($this->year >= 1960) {
+            $this->addHoliday(new Holiday('eidDay', [
+                'en' => 'Eid al-Fitr',
+            ], new DateTime("$this->year-04-22", DateTimeZoneFactory::getDateTimeZone($this->timezone)), $this->locale));
         }
     }
    
