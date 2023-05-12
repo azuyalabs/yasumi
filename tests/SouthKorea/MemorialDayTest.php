ef3c7b6de17e282d130ae849715b879f1c82c137
barami <barams@gmail.com>
2023-05-13 01:11:57 +0900

    Fixed an incorrect holiday year of enactment.

--- tests/SouthKorea/MemorialDayTest.php
+++ tests/SouthKorea/MemorialDayTest.php
@@ -31,7 +31,7 @@ class MemorialDayTest extends SouthKoreaBaseTestCase implements HolidayTestCase
     /**
      * The year in which the holiday was first established.
      */
-    public const ESTABLISHMENT_YEAR = 1966;
+    public const ESTABLISHMENT_YEAR = 1956;
 
     /**
      * Tests the holiday defined in this test.
