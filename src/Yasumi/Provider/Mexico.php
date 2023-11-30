9dadae6355aee930b2fd5a646e9723443665f8ba
Luis Gonzalez <luisarmando1234@gmail.com>
2023-11-30 18:53:59 -0500

    WIP Mexico Provider

---
+++ src/Yasumi/Provider/Mexico.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Yasumi package.
+ *
+ * Copyright (c) 2015 - 2023 AzuyaLabs
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author Sacha Telgenhof <me at sachatelgenhof dot com>
+ */
+
+namespace Yasumi\Provider;
+
+use Yasumi\Exception\UnknownLocaleException;
+use Yasumi\Holiday;
+
+/**
+ * Provider for all holidays in Mexico.
+ */
+class Mexico extends AbstractProvider
+{
+    use CommonHolidays;
+    use ChristianHolidays;
+
+    /**
+     * Code to identify this Holiday Provider. Typically, this is the ISO3166 code corresponding to the respective
+     * country or sub-region.
+     */
+    public const ID = 'MX';
+
+        /**
+     * Initialize holidays for the USA.
+     *
+     * @throws \InvalidArgumentException
+     * @throws UnknownLocaleException
+     * @throws \Exception
+     */
+    public function initialize(): void
+    {
+        $this->timezone = 'America/New_York';
+    }
+}
