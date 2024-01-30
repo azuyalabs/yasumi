84c263b3f5873f71336b3e1e61c43d9074ae2a7d
Sacha Telgenhof <me@sachatelgenhof.com>
2024-01-31 01:06:31 +0900

    build: use shared PHP CS Fixer config
    Signed-off-by: Sacha Telgenhof <me@sachatelgenhof.com>

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -12,29 +12,8 @@ declare(strict_types=1);
  *
  * @author Sacha Telgenhof <me at sachatelgenhof dot com>
  */
-$finder = PhpCsFixer\Finder::create()->in(__DIR__);
 
-$config = new PhpCsFixer\Config();
-$config->setRiskyAllowed(true)->setRules([
-  '@PER' => true,
-  '@Symfony' => true,
-  'combine_consecutive_issets' => true,
-  'combine_consecutive_unsets' => true,
-  'explicit_string_variable' => true,
-  'no_superfluous_elseif' => true,
-  'no_superfluous_phpdoc_tags' => ['remove_inheritdoc' => true],
-  'not_operator_with_successor_space' => true,
-  'nullable_type_declaration_for_default_null_value' => ['use_nullable_type_declaration' => true ],
-  'ordered_class_elements' => true,
-
-  // Risky
-  'declare_strict_types' => true,
-  'dir_constant' => true,
-  'get_class_to_class_keyword' => true,
-  'is_null' => true,
-  'modernize_strpos' => true,
-  'modernize_types_casting' => true,
-  'self_accessor' => true,
-])->setFinder($finder);
+$config = new AzuyaLabs\PhpCsFixerConfig\Config();
+$config->getFinder()->in(__DIR__)->notPath('var');
 
 return $config;
