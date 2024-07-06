8abe19f302e819e536ea9d70e9b8caf2b6fe494c
Reza Rabbani <50789773+thrashzone13@users.noreply.github.com>
2024-07-06 06:54:04 +0200

    feat: add Iran provider (#341)
    Implement provider for Iran

---
+++ tests/Iran/RevoltOfKhordad15Test.php
@@ -0,0 +1,76 @@
+<?php
+
+declare(strict_types = 1);
+
+/**
+ * This file is part of the 'Yasumi' package.
+ *
+ * The easy PHP Library for calculating holidays.
+ *
+ * Copyright (c) 2015 - 2024 AzuyaLabs
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author Sacha Telgenhof <me at sachatelgenhof dot com>
+ */
+
+namespace Yasumi\tests\Iran;
+
+use Yasumi\Holiday;
+use Yasumi\tests\HolidayTestCase;
+use Yasumi\Yasumi;
+
+class RevoltOfKhordad15Test extends IranBaseTestCase implements HolidayTestCase
+{
+    public const HOLIDAY = 'revoltOfKhordad15';
+
+    public const ESTABLISHMENT_YEAR = 1979;
+
+    public function testRevoltOfKhordad15BeforeEstablishment(): void
+    {
+        $this->assertNotHoliday(
+            self::REGION,
+            self::HOLIDAY,
+            $this->generateRandomYear(Yasumi::YEAR_LOWER_BOUND, self::ESTABLISHMENT_YEAR - 1)
+        );
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function testRevoltOfKhordad15AfterEstablishment(): void
+    {
+        $year = $this->generateRandomYear(self::ESTABLISHMENT_YEAR);
+        $this->assertHoliday(
+            self::REGION,
+            self::HOLIDAY,
+            $year,
+            new \DateTime("{$year}-06-05", new \DateTimeZone(self::TIMEZONE))
+        );
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function testTranslation(): void
+    {
+        $this->assertTranslatedHolidayName(
+            self::REGION,
+            self::HOLIDAY,
+            $this->generateRandomYear(self::ESTABLISHMENT_YEAR),
+            [
+                self::LOCALE => 'قیام ۱۵ خرداد',
+                'en' => 'Qiam e Panzdah e Khordad',
+            ]
+        );
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function testHolidayType(): void
+    {
+        $this->assertHolidayType(self::REGION, self::HOLIDAY, $this->generateRandomYear(self::ESTABLISHMENT_YEAR), Holiday::TYPE_OFFICIAL);
+    }
+}
---
+++ tests/Iran/SizdahBedarTest.php
@@ -0,0 +1,64 @@
+<?php
+
+declare(strict_types = 1);
+
+/**
+ * This file is part of the 'Yasumi' package.
+ *
+ * The easy PHP Library for calculating holidays.
+ *
+ * Copyright (c) 2015 - 2024 AzuyaLabs
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author Sacha Telgenhof <me at sachatelgenhof dot com>
+ */
+
+namespace Yasumi\tests\Iran;
+
+use Yasumi\Holiday;
+use Yasumi\tests\HolidayTestCase;
+
+class SizdahBedarTest extends IranBaseTestCase implements HolidayTestCase
+{
+    public const HOLIDAY = 'sizdahBedar';
+
+    /**
+     * @throws \Exception
+     */
+    public function testSizdahBedar(): void
+    {
+        $year = $this->generateRandomYear();
+        $this->assertHoliday(
+            self::REGION,
+            self::HOLIDAY,
+            $year,
+            new \DateTime("{$year}-04-02", new \DateTimeZone(self::TIMEZONE))
+        );
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function testTranslation(): void
+    {
+        $this->assertTranslatedHolidayName(
+            self::REGION,
+            self::HOLIDAY,
+            $this->generateRandomYear(),
+            [
+                self::LOCALE => 'سیزده بدر',
+                'en' => 'Sizdah be dar',
+            ]
+        );
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function testHolidayType(): void
+    {
+        $this->assertHolidayType(self::REGION, self::HOLIDAY, $this->generateRandomYear(), Holiday::TYPE_OFFICIAL);
+    }
+}
