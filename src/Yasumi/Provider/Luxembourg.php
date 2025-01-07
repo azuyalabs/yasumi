26c3455953c8c777432571f9146226416cff6484
Sacha Telgenhof <me@sachatelgenhof.com>
2025-01-08 00:48:11 +0900

    docs: update copyright year
    Signed-off-by: Sacha Telgenhof <me@sachatelgenhof.com>

--- src/Yasumi/Provider/Luxembourg.php
+++ src/Yasumi/Provider/Luxembourg.php
@@ -1,16 +1,5 @@
 <?php
 
-/*
- * This file is part of the Yasumi package.
- *
- * Copyright (c) 2015 - 2024 AzuyaLabs
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- *
- * @author Sacha Telgenhof <me at sachatelgenhof dot com>
- */
-
 declare(strict_types = 1);
 
 /**
@@ -18,7 +7,7 @@ declare(strict_types = 1);
  *
  * The easy PHP Library for calculating holidays.
  *
- * Copyright (c) 2015 - 2024 AzuyaLabs
+ * Copyright (c) 2015 - 2025 AzuyaLabs
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
