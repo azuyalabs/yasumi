75d684a431ae083de2c3b732466cd7b20c992f2d
Christian Schmidt <github@chsc.dk>
2025-03-15 08:22:17 +0100

    fix(mexico): mark several holidays as observance (#362)
    AFAICT, these holidays are not official in Mexico, at least not according to the Mexican Federal Labor Law (Ley Federal del Trabajo):
    
    - Epiphany
    - Good Friday
    - Assumption of Mary
    - All Saints Day
    - Immaculate Conception
    - Virgin of Guadalupe
    - Christmas Eve
    - New Year's Eve
    
    I don't know which other legislation would make these official.
    I have marked them `TYPE_OBSERVANCE` instead. I am not sure how exactly the different types are defined, though.
    
    Co-authored-by: Sacha Telgenhof <me@sachatelgenhof.com>

--- tests/Mexico/VirginOfGuadalupeTest.php
+++ tests/Mexico/VirginOfGuadalupeTest.php
@@ -70,6 +70,6 @@ class VirginOfGuadalupeTest extends MexicoBaseTestCase implements HolidayTestCas
     public function testHolidayType(): void
     {
         $year = $this->generateRandomYear(self::ESTABLISHMENT_YEAR);
-        $this->assertHolidayType(self::REGION, self::HOLIDAY, $year, Holiday::TYPE_OFFICIAL);
+        $this->assertHolidayType(self::REGION, self::HOLIDAY, $year, Holiday::TYPE_OBSERVANCE);
     }
 }
