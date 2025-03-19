88a537ace2370610a15c8305cfc2412b6bfc0b33
Christian Schmidt <github@chsc.dk>
2025-03-20 00:06:01 +0100

    feat(ireland): Saint Brigid's Day (#374)
    Saint Brigid's Day has been a public holiday in Ireland since 2023.
    
    Sources:
    https://www.gov.ie/en/press-release/b926b-government-agrees-covid-recognition-payment-and-new-public-holiday/
    https://www.irishstatutebook.ie/eli/2022/si/50/made/en/html
    https://en.wikipedia.org/wiki/Imbolc
    
    Also, the Holidays (Employees) Act, 1973, has been repealed and replaced by the Organisation of Working Time Act, 1993. I have updated references accordingly.
    
    Source:
    https://www.irishstatutebook.ie/eli/1997/act/20/schedule/4/enacted/en/html
    
    Signed-off-by: Christian Schmidt <github@chsc.dk>

---
+++ tests/Ireland/StBrigidsDayTest.php
@@ -0,0 +1,132 @@
+<?php
+
+declare(strict_types = 1);
+
+/**
+ * This file is part of the 'Yasumi' package.
+ *
+ * The easy PHP Library for calculating holidays.
+ *
+ * Copyright (c) 2015 - 2025 AzuyaLabs
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author Sacha Telgenhof <me at sachatelgenhof dot com>
+ */
+
+namespace Yasumi\tests\Ireland;
+
+use Yasumi\Holiday;
+use Yasumi\tests\HolidayTestCase;
+
+/**
+ * Class for testing the Saint Brigid's Day in Ireland.
+ */
+class StBrigidsDayTest extends IrelandBaseTestCase implements HolidayTestCase
+{
+    /**
+     * The name of the holiday.
+     */
+    public const HOLIDAY = 'stBrigidsDay';
+
+    /**
+     * The year in which the holiday was first established.
+     */
+    public const ESTABLISHMENT_YEAR = 2023;
+
+    /**
+     * Tests the holiday defined in this test.
+     *
+     * @dataProvider HolidayDataProvider
+     *
+     * @param int    $year     the year for which the holiday defined in this test needs to be tested
+     * @param string $expected the expected date
+     *
+     * @throws \Exception
+     */
+    public function testHoliday(int $year, string $expected): void
+    {
+        $dateTime = new \DateTime($expected, new \DateTimeZone(self::TIMEZONE));
+        $this->assertHoliday(
+            self::REGION,
+            self::HOLIDAY,
+            $year,
+            $dateTime
+        );
+    }
+
+    /**
+     * Returns a list of test dates.
+     *
+     * @return array<array> list of test dates for the holiday defined in this test
+     *
+     * @throws \Exception
+     */
+    public function HolidayDataProvider(): array
+    {
+        $data = [];
+
+        for ($y = 0; $y < self::TEST_ITERATIONS; ++$y) {
+            $year = $this->generateRandomYear(self::ESTABLISHMENT_YEAR);
+            $date = new \DateTime("{$year}-02-01", new \DateTimeZone(self::TIMEZONE));
+            if ('Friday' !== $date->format('l')) {
+                $date = new \DateTime("{$year}-02-01 first monday", new \DateTimeZone(self::TIMEZONE));
+            }
+
+            $data[] = [$year, $date->format('Y-m-d')];
+        }
+
+        return $data;
+    }
+
+    /**
+     * Tests the holiday defined in this test before establishment.
+     *
+     * @throws \Exception
+     */
+    public function testHolidayBeforeEstablishment(): void
+    {
+        $this->assertNotHoliday(
+            self::REGION,
+            self::HOLIDAY,
+            $this->generateRandomYear(1000, self::ESTABLISHMENT_YEAR - 1)
+        );
+    }
+
+    /**
+     * Tests the translated name of the holiday defined in this test.
+     *
+     * @throws \Exception
+     */
+    public function testTranslation(): void
+    {
+        $this->assertTranslatedHolidayName(
+            self::REGION,
+            self::HOLIDAY,
+            $this->generateRandomYear(self::ESTABLISHMENT_YEAR),
+            [self::LOCALE => 'Saint Brigid’s Day']
+        );
+        $this->assertTranslatedHolidayName(
+            self::REGION,
+            self::HOLIDAY,
+            $this->generateRandomYear(self::ESTABLISHMENT_YEAR),
+            ['ga' => 'Lá Fhéile Bríde']
+        );
+    }
+
+    /**
+     * Tests type of the holiday defined in this test.
+     *
+     * @throws \Exception
+     */
+    public function testHolidayType(): void
+    {
+        $this->assertHolidayType(
+            self::REGION,
+            self::HOLIDAY,
+            $this->generateRandomYear(self::ESTABLISHMENT_YEAR),
+            Holiday::TYPE_OFFICIAL
+        );
+    }
+}
