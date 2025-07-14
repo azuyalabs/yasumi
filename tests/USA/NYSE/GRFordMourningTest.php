bea83ed5af9faef3a2e2c4be4b96364fa7682e8d
Art Kurbakov <admin@mgwebgroup.com>
2025-07-14 03:51:12 +0000

    Added tests for closures due to weather, emergencies and proclamations
    Signed-off-by: Art Kurbakov <admin@mgwebgroup.com>

---
+++ tests/USA/NYSE/GRFordMourningTest.php
@@ -0,0 +1,83 @@
+<?php
+
+declare(strict_types = 1);
+
+/**
+ * This file is part of the 'Yasumi' package.
+ *
+ * The easy PHP Library for calculating holidays.
+ *
+ * Copyright (c) 2025 Magic Web Group
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author Art Kurbakov <admin at mgwebgroup dot com>
+ */
+
+namespace Yasumi\tests\USA\NYSE;
+
+use Yasumi\Holiday;
+use Yasumi\tests\HolidayTestCase;
+use Yasumi\tests\USA\USABaseTestCase;
+use DateTime;
+use DateTimeZone;
+
+/**
+ * Class to test day of closure of NYSE due to G.R. Ford Mourning proclamation
+ */
+class GRFordMourningTest extends USABaseTestCase
+{
+    /**
+     * Name of provider to be tested.
+     */
+    public const REGION = 'USA/NYSE';
+
+    /**
+     * The year when the closure was observed.
+     */
+    public const OBSERVED_YEAR = 2007;
+
+    /**
+     * Tests day of closure on January 2nd 2007
+     *
+     * @throws \Exception
+     */
+    public function testGRFordMourning(): void
+    {
+        $this->assertHoliday(
+            self::REGION,
+            'GRFordMourning',
+            self::OBSERVED_YEAR,
+            new DateTime("2007-01-02", new DateTimeZone(self::TIMEZONE))
+        );
+    }
+
+    /**
+     * Tests years before 2007
+     *
+     * @throws \Exception
+     */
+    public function testGRFordMourningBefore2007(): void
+    {
+        $this->assertNotHoliday(
+           	self::REGION,
+            'GRFordMourning',
+            self::OBSERVED_YEAR - 1
+        );
+    }
+
+    /**
+     * Tests years after 2007
+     *
+     * @throws \Exception
+     */
+    public function testGRFordMourningAfter2007(): void
+    {
+        $this->assertNotHoliday(
+           	self::REGION,
+            'GRFordMourning',
+            self::OBSERVED_YEAR + 1
+        );
+    }
+}
---
+++ tests/USA/NYSE/GroundZeroTest.php
@@ -0,0 +1,140 @@
+<?php
+
+declare(strict_types = 1);
+
+/**
+ * This file is part of the 'Yasumi' package.
+ *
+ * The easy PHP Library for calculating holidays.
+ *
+ * Copyright (c) 2025 Magic Web Group
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author Art Kurbakov <admin at mgwebgroup dot com>
+ */
+
+namespace Yasumi\tests\USA\NYSE;
+
+use Yasumi\Holiday;
+use Yasumi\tests\HolidayTestCase;
+use Yasumi\tests\USA\USABaseTestCase;
+use DateTime;
+use DateTimeZone;
+
+/**
+ * Class to test 4 days of closure of NYSE due to September 11th attacks
+ */
+class GroundZeroTest extends USABaseTestCase
+{
+    /**
+     * Name of provider to be tested.
+     */
+    public const REGION = 'USA/NYSE';
+
+    /**
+     * The year when the closure was observed.
+     */
+    public const OBSERVED_YEAR = 2001;
+
+    /**
+     * Tests 4 days of closure on September 11th 2001
+     *
+     * @throws \Exception
+     */
+    public function testGroundZero(): void
+    {
+        $this->assertHoliday(
+            self::REGION,
+            'groundZero1',
+            self::OBSERVED_YEAR,
+            new DateTime("2001-09-11", new DateTimeZone(self::TIMEZONE))
+        );
+
+        $this->assertHoliday(
+            self::REGION,
+            'groundZero2',
+            self::OBSERVED_YEAR,
+            new DateTime("2001-09-12", new DateTimeZone(self::TIMEZONE))
+        );
+
+        $this->assertHoliday(
+            self::REGION,
+            'groundZero3',
+            self::OBSERVED_YEAR,
+            new DateTime("2001-09-13", new DateTimeZone(self::TIMEZONE))
+        );
+
+        $this->assertHoliday(
+            self::REGION,
+            'groundZero4',
+            self::OBSERVED_YEAR,
+            new DateTime("2001-09-14", new DateTimeZone(self::TIMEZONE))
+        );
+    }
+
+    /**
+     * Tests years before 2001  
+     *
+     * @throws \Exception
+     */
+    public function testGroundZeroBefore2001(): void
+    {
+        $this->assertNotHoliday(
+           	self::REGION,
+            'groundZero1',
+            self::OBSERVED_YEAR - 1
+        );
+
+        $this->assertNotHoliday(
+           	self::REGION,
+            'groundZero2',
+            self::OBSERVED_YEAR - 1
+        );
+
+        $this->assertNotHoliday(
+           	self::REGION,
+            'groundZero3',
+            self::OBSERVED_YEAR - 1
+        );
+
+        $this->assertNotHoliday(
+           	self::REGION,
+            'groundZero4',
+            self::OBSERVED_YEAR - 1
+        );
+    }
+
+    /**
+     * Tests years after 2001  
+     *
+     * @throws \Exception
+     */
+    public function testGroundZeroAfter2001(): void
+    {
+        $this->assertNotHoliday(
+           	self::REGION,
+            'groundZero1',
+            self::OBSERVED_YEAR + 1
+        );
+
+        $this->assertNotHoliday(
+           	self::REGION,
+            'groundZero2',
+            self::OBSERVED_YEAR + 1
+        );
+
+        $this->assertNotHoliday(
+           	self::REGION,
+            'groundZero3',
+            self::OBSERVED_YEAR + 1
+        );
+
+        $this->assertNotHoliday(
+           	self::REGION,
+            'groundZero4',
+            self::OBSERVED_YEAR + 1
+        );
+    }
+}
