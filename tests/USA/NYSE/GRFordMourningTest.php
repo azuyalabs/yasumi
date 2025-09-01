c3ff8b5ef4381ed8c44be8aad89b6c8b76332aa6
Art Kurbakov <mgwebgroup@users.noreply.github.com>
2025-09-01 10:41:26 +0000

    feat: Add New York Stock Exchange (NYSE) provider (#384)
    Added a holiday provider to track holidays and closures for the New York
    Stock Exchange. In addition to observed holidays, the closures include
    emergency, weather and presidential proclamation events since year 2000.
    Other US exchanges like NASDAQ and CBOE normally follow suit for
    holidays and closures.
    
    This PR supersedes PR #382.
    
    ---------
    
    Signed-off-by: Art Kurbakov <admin@mgwebgroup.com>
    Co-authored-by: Art Kurbakov <admin@mgwebgroup.com>
    Signed-off-by: Ján Hamrák <hamrak@kcorp.sk>

---
+++ tests/USA/NYSE/GRFordMourningTest.php
@@ -0,0 +1,81 @@
+<?php
+
+declare(strict_types = 1);
+
+/**
+ * This file is part of the 'Yasumi' package.
+ *
+ * The easy PHP Library for calculating holidays.
+ *
+ * Copyright (c) 2015 - 2025 AzuyaLabs
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author Sacha Telgenhof <me at sachatelgenhof dot com>
+ */
+
+namespace Yasumi\tests\USA\NYSE;
+
+use Yasumi\tests\USA\USABaseTestCase;
+
+/**
+ * Class to test day of closure of NYSE due to G.R. Ford Mourning proclamation
+ *
+ * @author Art Kurbakov <admin at mgwebgroup dot com>
+ */
+class GRFordMourningTest extends USABaseTestCase
+{
+    /**
+     * Name of provider to be tested.
+     */
+    public const REGION = 'USA/NYSE';
+
+    /**
+     * The year when the closure was observed.
+     */
+    public const OBSERVED_YEAR = 2007;
+
+    /**
+     * Tests day of closure on January 2nd 2007
+     *
+     * @throws \Exception
+     */
+    public function testGRFordMourning(): void
+    {
+        $this->assertHoliday(
+            self::REGION,
+            'GRFordMourning',
+            self::OBSERVED_YEAR,
+            new \DateTime('2007-01-02', new \DateTimeZone(self::TIMEZONE))
+        );
+    }
+
+    /**
+     * Tests years before 2007
+     *
+     * @throws \Exception
+     */
+    public function testGRFordMourningBefore2007(): void
+    {
+        $this->assertNotHoliday(
+            self::REGION,
+            'GRFordMourning',
+            self::OBSERVED_YEAR - 1
+        );
+    }
+
+    /**
+     * Tests years after 2007
+     *
+     * @throws \Exception
+     */
+    public function testGRFordMourningAfter2007(): void
+    {
+        $this->assertNotHoliday(
+            self::REGION,
+            'GRFordMourning',
+            self::OBSERVED_YEAR + 1
+        );
+    }
+}
