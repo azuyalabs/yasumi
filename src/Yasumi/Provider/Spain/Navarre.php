f5b9b0d9c9aab23d3e4b6cedd2a59b9a50e29667
Sacha Telgenhof <me@sachatelgenhof.com>
2026-01-15 22:00:59 +0900

    docs: update copyright year
    Signed-off-by: Sacha Telgenhof <me@sachatelgenhof.com>

--- src/Yasumi/Provider/Spain/Navarre.php
+++ src/Yasumi/Provider/Spain/Navarre.php
@@ -1,11 +1,13 @@
 <?php
 
-declare(strict_types=1);
+declare(strict_types = 1);
 
-/*
- * This file is part of the Yasumi package.
+/**
+ * This file is part of the 'Yasumi' package.
  *
- * Copyright (c) 2015 - 2025 AzuyaLabs
+ * The easy PHP Library for calculating holidays.
+ *
+ * Copyright (c) 2015 - 2026 AzuyaLabs
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
--- tests/Switzerland/AppenzellAusserrhoden/GoodFridayTest.php
+++ tests/Switzerland/AppenzellAusserrhoden/GoodFridayTest.php
@@ -7,7 +7,7 @@ declare(strict_types = 1);
  *
  * The easy PHP Library for calculating holidays.
  *
- * Copyright (c) 2015 - 2025 AzuyaLabs
+ * Copyright (c) 2015 - 2026 AzuyaLabs
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
