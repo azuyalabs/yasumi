54b089621a74336865f9622ec890632fab3b5067
Pascal Paulis <ppaulis@gmail.com>
2026-02-06 10:49:44 +0100

    added "Czechia" provider as a short form of "Czech Republic"
    Signed-off-by: Pascal Paulis <ppaulis@gmail.com>

---
+++ src/Yasumi/Provider/Czechia.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types = 1);
+
+/**
+ * This file is part of the 'Yasumi' package.
+ *
+ * The easy PHP Library for calculating holidays.
+ *
+ * Copyright (c) 2015 - 2026 AzuyaLabs
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author Sacha Telgenhof <me at sachatelgenhof dot com>
+ */
+
+namespace Yasumi\Provider;
+
+/**
+ * Provider for all holidays in Czechia. "Czechia" is the official short-name for "Czech Republic".
+ *
+ * Class Czechia
+ *
+ * @author  Pascal Paulis <ppaulis@gmail.com>
+ */
+class Czechia extends CzechRepublic
+{
+}
