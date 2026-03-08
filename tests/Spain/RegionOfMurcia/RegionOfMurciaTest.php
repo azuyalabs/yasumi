985396164f00472e121f82195b2235398e22fdb7
Sacha Telgenhof <me@sachatelgenhof.com>
2026-03-08 10:53:10 +0900

    test(spain): fix murciaDay minimum year in RegionOfMurcia test
    The `generateRandomYear(1981)` function could produce years before 1983, but
    `murciaDay` is only added from 1983 (first celebration).
    
    Signed-off-by: Sacha Telgenhof <me@sachatelgenhof.com>

--- tests/Spain/RegionOfMurcia/RegionOfMurciaTest.php
+++ tests/Spain/RegionOfMurcia/RegionOfMurciaTest.php
@@ -37,7 +37,7 @@ class RegionOfMurciaTest extends RegionOfMurciaBaseTestCase implements ProviderT
      */
     protected function setUp(): void
     {
-        $this->year = static::generateRandomYear(1981);
+        $this->year = static::generateRandomYear(1983);
     }
 
     /**
