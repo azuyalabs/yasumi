c9beb82d7f2fce984573d59381b8cb35be4ce7c0
barami <barams@gmail.com>
2026-05-09 10:57:53 +0000

    Add translation for arbor day
    Signed-off-by: barami <barams@gmail.com>

---
+++ src/Yasumi/data/translations/arborDay.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types = 1);
+
+/**
+ * This file is part of the 'Yasumi' package.
+ *
+ * The easy PHP Library for calculating holidays.
+ *
+ * Copyright (c) 2015 - 2026 AzuyaLabs
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author Sacha Telgenhof <me at sachatelgenhof dot com>
+ */
+
+// Translations for Arbor Day
+return [
+    'en' => 'Arbor Day',
+    'ko' => '식목일',
+];
