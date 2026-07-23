6ac266db0145e5192701d606e0766c2a7e809632
barami <barams@gmail.com>
2026-07-23 22:30:16 +0900

    initial tests for following classes - Yasumi\Provider\SouthKorea\Policy\SubstitutePolicy - Yasumi\Provider\SouthKorea\Translation\KoreanTranslation
    Signed-off-by: barami <barams@gmail.com>

---
+++ tests/SouthKorea/Policy/SubstitutePolicyTest.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types = 1);
+
+/**
+ * This file is part of the 'Yasumi' package.
+ *
+ * The easy PHP Library for calculating holidays.
+ *
+ * Copyright (c) 2015 - 2026 AzuyaLabs
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author Sacha Telgenhof <me at sachatelgenhof dot com>
+ */
+
+namespace Yasumi\tests\SouthKorea\Policy;
+
+use Yasumi\tests\SouthKorea\SouthKoreaBaseTestCase;
+
+class SubstitutePolicyTest extends SouthKoreaBaseTestCase
+{
+}
