696485872dda1a85a64479ab44d3c0764a6c595e
barami <barams@gmail.com>
2026-07-24 23:51:26 +0900

    Test case for SubstitutePolicy class
    Signed-off-by: barami <barams@gmail.com>

--- tests/SouthKorea/Policy/SubstitutePolicyTest.php
+++ tests/SouthKorea/Policy/SubstitutePolicyTest.php
@@ -17,8 +17,46 @@ declare(strict_types = 1);
 
 namespace Yasumi\tests\SouthKorea\Policy;
 
+use PHPUnit\Framework\Attributes\DataProvider;
+use Yasumi\Provider\SouthKorea\Policy\SubstitutePolicy;
 use Yasumi\tests\SouthKorea\SouthKoreaBaseTestCase;
+use Yasumi\Yasumi;
 
 class SubstitutePolicyTest extends SouthKoreaBaseTestCase
 {
+    /**
+     * The name of the holiday.
+     */
+    public const HOLIDAY = 'liberationDay';
+
+    public const YEAR_LOWER_BOUND = 2023;
+
+    public const YEAR_UPPER_BOUND = 9999;
+
+    /**
+     * Tests whether the function that determines the substitute holiday condition works properly.
+     *
+     * @throws \Exception
+     */
+    #[DataProvider('YearsDataProvider')]
+    public function testSubstituteHoliday(int $year): void
+    {
+        $holiday = Yasumi::create(self::REGION, $year)->getHoliday(self::HOLIDAY);
+
+        $policy = new SubstitutePolicy($year);
+        $this->assertTrue($policy->canSubsitute($holiday));
+
+        if (self::isWeekend($holiday)) {
+            $this->assertTrue($policy->shouldSubstitute($holiday));
+        } else {
+            $this->assertFalse($policy->shouldSubstitute($holiday));
+        }
+    }
+
+    public static function YearsDataProvider(): \Generator
+    {
+        for ($i = 0; $i < 20; ++$i) {
+            yield [static::numberBetween(self::YEAR_LOWER_BOUND, self::YEAR_UPPER_BOUND)];
+        }
+    }
 }
