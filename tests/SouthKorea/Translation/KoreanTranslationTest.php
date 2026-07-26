f0d7895f1c3d6154e90509b66a27810b1f3c9ac6
barami <barams@gmail.com>
2026-07-27 01:32:16 +0900

    Test case for KoreanTranslation class
    Signed-off-by: barami <barams@gmail.com>

--- tests/SouthKorea/Translation/KoreanTranslationTest.php
+++ tests/SouthKorea/Translation/KoreanTranslationTest.php
@@ -17,8 +17,67 @@ declare(strict_types = 1);
 
 namespace Yasumi\tests\SouthKorea\Translation;
 
+use PHPUnit\Framework\Attributes\DataProvider;
+use Yasumi\Provider\SouthKorea\Translation\KoreanTranslation;
 use Yasumi\tests\SouthKorea\SouthKoreaBaseTestCase;
 
 class KoreanTranslationTest extends SouthKoreaBaseTestCase
 {
+    /**
+     * The name of the holiday.
+     */
+    public const HOLIDAY = 'liberationDay';
+
+    public const YEAR_LOWER_BOUND = 2023;
+
+    public const YEAR_UPPER_BOUND = 9999;
+
+    /**
+     * Tests injecting translation data into the class constructor.
+     *
+     * @throws \Exception
+     */
+    #[DataProvider('YearsDataProvider')]
+    public function testConstructor(int $year): void
+    {
+        $translation = new KoreanTranslation($year, []);
+        $this->assertEmpty($translation->getTranslations(self::HOLIDAY));
+
+        $translations[self::HOLIDAY]['en'] = 'Liberation Day';
+        $translations[self::HOLIDAY]['ko'] = '광복절';
+
+        $translation = new KoreanTranslation($year, $translations);
+        $this->assertArrayIsIdenticalToArrayOnlyConsideringListOfKeys($translations[self::HOLIDAY], $translation->getTranslations(self::HOLIDAY), ['en']);
+
+        $this->assertSame('Liberation Day', $translation->getTranslations(self::HOLIDAY)['en']);
+        $this->assertSame('광복절', $translation->getTranslations(self::HOLIDAY)['ko']);
+    }
+
+    /**
+     * Test method functionality such as lookup after adding translations.
+     *
+     * @throws \Exception
+     */
+    #[DataProvider('YearsDataProvider')]
+    public function testMethods(int $year): void
+    {
+        $translation = new KoreanTranslation($year, []);
+        $this->assertEmpty($translation->getTranslations(self::HOLIDAY));
+
+        $translation->addTranslation(self::HOLIDAY, 'Liberation Day', 'en');
+        $translation->addTranslation(self::HOLIDAY, '광복절', 'ko');
+
+        $this->assertNotEmpty($translation->getTranslations(self::HOLIDAY));
+        $this->assertArrayIsIdenticalToArrayOnlyConsideringListOfKeys([
+            'en' => 'Liberation Day',
+            'ko' => '광복절',
+        ], $translation->getTranslations(self::HOLIDAY), ['en', 'ko']);
+    }
+
+    public static function YearsDataProvider(): \Generator
+    {
+        for ($i = 0; $i < 20; ++$i) {
+            yield [static::numberBetween(self::YEAR_LOWER_BOUND, self::YEAR_UPPER_BOUND)];
+        }
+    }
 }
